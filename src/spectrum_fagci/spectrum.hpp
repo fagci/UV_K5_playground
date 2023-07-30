c74de2a2879e1e96ee55ae338782c0a8afd3b743
Mikhail Yudin <fagci.nsk@gmail.com>
2023-07-30 23:38:28 +0700

    feat: trying to rewrite measurement

--- src/spectrum_fagci/spectrum.hpp
+++ src/spectrum_fagci/spectrum.hpp
@@ -2,14 +2,7 @@
 #include "radio.hpp"
 #include "system.hpp"
 #include "uv_k5_display.hpp"
-
-typedef unsigned char u8;
-typedef signed short i16;
-typedef unsigned short u16;
-typedef signed int i32;
-typedef unsigned int u32;
-typedef signed long long i64;
-typedef unsigned long long u64;
+#include "types.hpp"
 
 template <const System::TOrgFunctions &Fw, const System::TOrgData &FwData,
           Radio::CBK4819<Fw> &RadioDriver>
